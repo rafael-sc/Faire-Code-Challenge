86129036c4c3656a59ec354ff335ce89c561a02b
Rafael <rafael.costa.ti@gmail.com>
2023-02-06 22:54:34 -0300

    adding resourceprovider to WeatherRepositoryImpl.kt

--- app/src/main/java/com/orafaelsc/fairetest/data/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/orafaelsc/fairetest/data/WeatherRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.orafaelsc.fairetest.data
 
+import com.orafaelsc.fairetest.commom.ResourceProvider
 import com.orafaelsc.fairetest.commom.exceptions.ApiException
 import com.orafaelsc.fairetest.data.network.WeatherApi
 import com.orafaelsc.fairetest.domain.extensions.toDomain
@@ -7,13 +8,14 @@ import com.orafaelsc.fairetest.domain.repository.WeatherRepository
 import com.orafaelsc.fairetest.ui.forecast.ForecastViewObject
 
 class WeatherRepositoryImpl(
-    private val weatherApi: WeatherApi
+    private val weatherApi: WeatherApi,
+    private val resourceProvider: ResourceProvider,
 ) : WeatherRepository {
 
     override suspend fun getWeatherForecast(locationId: String): ForecastViewObject {
         val result = weatherApi.getWeatherForecast(locationId)
         if (result.isSuccessful) {
-            return result.body()?.toDomain()
+            return result.body()?.toDomain(resourceProvider)
                 ?: throw ApiException.UnableToGetWeatherForecastException()
         } else {
             throw ApiException.UnableToGetWeatherForecastException()
--- app/src/main/java/com/orafaelsc/fairetest/domain/extensions/WeatherResponseExtensions.kt
+++ app/src/main/java/com/orafaelsc/fairetest/domain/extensions/WeatherResponseExtensions.kt
@@ -1,10 +1,11 @@
 package com.orafaelsc.fairetest.domain.extensions
 
+import com.orafaelsc.fairetest.commom.ResourceProvider
 import com.orafaelsc.fairetest.data.network.model.WeatherResponse
 import com.orafaelsc.fairetest.ui.forecast.ForecastViewObject
 import kotlin.math.roundToInt
 
-fun WeatherResponse.toDomain() = ForecastViewObject(
+fun WeatherResponse.toDomain(resourceProvider: ResourceProvider) = ForecastViewObject(
     cityName = title,
     imageUrl = "https://cdn.faire.com/static/mobile-take-home/icons/${consolidated_weather[0].weather_state_abbr}.png",
     actualTemp = consolidated_weather[0].the_temp.roundToInt().toString(),
