138da825bf57d83556c705537c19e6f128bdb792
Rafael <rafael.costa.ti@gmail.com>
2023-02-06 23:12:24 -0300

    using strings to set ForecastViewObject data in WeatherResponseExtensions.kt

--- app/src/main/java/com/orafaelsc/fairetest/domain/extensions/WeatherResponseExtensions.kt
+++ app/src/main/java/com/orafaelsc/fairetest/domain/extensions/WeatherResponseExtensions.kt
@@ -1,5 +1,6 @@
 package com.orafaelsc.fairetest.domain.extensions
 
+import com.orafaelsc.fairetest.R
 import com.orafaelsc.fairetest.commom.ResourceProvider
 import com.orafaelsc.fairetest.data.network.model.WeatherResponse
 import com.orafaelsc.fairetest.ui.forecast.ForecastViewObject
@@ -7,9 +8,14 @@ import kotlin.math.roundToInt
 
 fun WeatherResponse.toDomain(resourceProvider: ResourceProvider) = ForecastViewObject(
     cityName = title,
-    imageUrl = "https://cdn.faire.com/static/mobile-take-home/icons/${consolidated_weather[0].weather_state_abbr}.png",
-    actualTemp = consolidated_weather[0].the_temp.roundToInt().toString(),
-    higherTemp = consolidated_weather[0].max_temp.roundToInt().toString(),
-    lowerTemp = consolidated_weather[0].min_temp.roundToInt().toString(),
+    imageUrl = resourceProvider.getString(
+        R.string.base_image_url,
+        consolidated_weather[0].weather_state_abbr
+    ),
+    actualTemp = resourceProvider.getString(R.string.actual_temp, consolidated_weather[0].the_temp.roundToInt()),
+    higherTemp = resourceProvider.getString(R.string.max_temp, consolidated_weather[0].max_temp.roundToInt()),
+    lowerTemp = resourceProvider.getString(R.string.min_temp, consolidated_weather[0].min_temp.roundToInt()),
     weatherStateName = consolidated_weather[0].weather_state_name
 )
+
+
